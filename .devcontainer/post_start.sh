f53aacfaa0ab1dbf87f365cb97fa4eb84023ee12
Vinicius Motta Garcia <vinicius.motta@hardware.org.br>
2026-02-19 12:40:21 +0000

    feat: added CV2 and Pyside6
    Added new librarys to use inside the container. Added configurations for webcam use and display use for both library

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -2,10 +2,31 @@
 
 echo "🚀 ROS 2 dev container iniciado"
 
-# Fonte do ROS base
+# Source ROS base
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-# Fonte do workspace (se existir)
+# Source workspace (se existir)
 if [ -f /home/ws/install/setup.bash ]; then
   source /home/ws/install/setup.bash
 fi
+
+echo "🔎 Verificando bibliotecas..."
+
+# Função para testar import
+check_import() {
+    MODULE=$1
+    python3 -c "import $MODULE" 2>/dev/null
+    if [ $? -eq 0 ]; then
+        echo "✅ $MODULE OK"
+    else
+        echo "❌ $MODULE NÃO ENCONTRADO"
+    fi
+}
+
+# Liste aqui todas suas libs importantes
+check_import rclpy
+check_import PySide6
+check_import cv2
+check_import numpy
+
+echo "🎯 Ambiente pronto."
